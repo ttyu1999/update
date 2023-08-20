8b7e9fd61e1542dee0e06322ae56f06208b2b756
ttyu1999 <love0033621@gmail.com>
2023-08-21 07:02:55 +0800

    修正選單按鈕的路徑設定

--- update/src/Component/Layout/Header/NavMenu.js
+++ update/src/Component/Layout/Header/NavMenu.js
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import { useCallback } from "react";
 import { HiOutlineChevronRight } from "react-icons/hi";
 import { HiOutlineChevronLeft } from "react-icons/hi";
@@ -7,54 +7,66 @@ import MENU_DATA from "../../../assets/menu-data";
 import useFindMenuItem from "../../../hook/useFindMenuItem";
 
 const NavMenu = (props) => {
+  const { onHide } = props;
+  const { categoriesClick } = useFindMenuItem();
 
-    const { onHide } = props;
-    const { categoriesClick } = useFindMenuItem();
+  const renderListItem = useCallback(
+    (menu) => {
+      return (
+        <li key={menu.id}>
+          <Link
+            to={`/product/${menu.id}`}
+            onClick={() => categoriesClick(menu.id, onHide)}
+          >
+            {menu.name}
+          </Link>
+          {menu.subMenus && (
+            <>
+              <input type="checkbox" id={menu.id} />
+              <label htmlFor={menu.id}>
+                <span className="icon">
+                  <HiOutlineChevronRight />
+                </span>
+              </label>
+            </>
+          )}
+          {menu.subMenus && (
+            <div className="drop__down" key={menu.id}>
+              <header className="menu__header">
+                <input type="checkbox" id={menu.id} />
+                <label htmlFor={menu.id}>
+                  <span className="icon back__prev">
+                    <HiOutlineChevronLeft />
+                  </span>
+                </label>
+                <h3>{menu.name}</h3>
+                <span className="icon menu__close" onClick={onHide}>
+                  <HiX />
+                </span>
+              </header>
+              <ul>
+                {menu.subMenus &&
+                  menu.subMenus?.map((subMenu) => {
+                    return renderListItem(subMenu);
+                  })}
+              </ul>
+            </div>
+          )}
+        </li>
+      );
+    },
+    [onHide, categoriesClick]
+  );
 
-    const renderListItem = useCallback((menu) => {
-        return (
-            <li key={menu.id}>
-                <button type="button" onClick={() => categoriesClick(menu.id, onHide)}>{menu.name}</button>
-                {menu.subMenus && 
-                <>
-                    <input type="checkbox" id={menu.id} />
-                    <label htmlFor={menu.id}>
-                        <span className="icon">
-                            <HiOutlineChevronRight />
-                        </span>
-                    </label>
-                </>
-                }
-                {menu.subMenus && 
-                <div className="drop__down" key={menu.id}>
-                    <header className="menu__header">
-                        <input type="checkbox" id={menu.id} />
-                        <label htmlFor={menu.id}>
-                            <span className="icon back__prev">
-                                <HiOutlineChevronLeft />
-                            </span>
-                        </label>
-                        <h3>{menu.name}</h3>
-                        <span className="icon menu__close" onClick={onHide}><HiX /></span>
-                    </header>
-                    <ul>
-                        {menu.subMenus && menu.subMenus?.map(subMenu => {
-                            return renderListItem(subMenu);
-                        })}
-                    </ul>
-                </div>
-                }
-            </li> 
-        );
-    }, [onHide, categoriesClick]);
-
-    return (
-        <ul>
-            <li>
-                <Link to="/" onClick={() => categoriesClick(null, onHide)}>首頁</Link>
-            </li>
-            {MENU_DATA.map(menu => renderListItem(menu, menu.subMenus))}
-        </ul>
-    )
-}
+  return (
+    <ul>
+      <li>
+        <Link to="/" onClick={() => categoriesClick(null, onHide)}>
+          首頁
+        </Link>
+      </li>
+      {MENU_DATA.map((menu) => renderListItem(menu, menu.subMenus))}
+    </ul>
+  );
+};
 export default NavMenu;
